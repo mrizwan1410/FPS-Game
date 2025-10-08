99bbcf4ec068b8140e2636135b9ee5aacc1ce1da
RIZWAN <morizwan1410@gmail.com>
2025-10-09 01:42:11 +0530

    Shooting Input

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] int startingHealth = 3;
+    int currentHealth;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] int damage = 1;
     StarterAssetsInputs starterAssetsInputs;
 
     private void Awake()
@@ -12,15 +13,24 @@ public class Weapon : MonoBehaviour
 
     void Update()
     {
-        if(starterAssetsInputs.shoot)
-        {
-            RaycastHit hit;
+        HandleShoot();
+    }
 
-            if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
-            {
-                Debug.Log(hit.collider.name);
-                starterAssetsInputs.ShootInput(false);
-            }
+    void HandleShoot()
+    {
+        if (!starterAssetsInputs.shoot) return;
+        
+        RaycastHit hit;
+
+        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+        {
+            EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
+            enemyHealth?.TakeDamage(damage);
+             //if (enemyHealth)
+             //{
+             //    enemyHealth.TakeDamage(damage);
+             //}
+            starterAssetsInputs.ShootInput(false);
         }
     }
 }
