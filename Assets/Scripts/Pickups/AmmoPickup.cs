ebd515204e680ca542d337cc5217f79f6600ea15
RIZWAN <morizwan1410@gmail.com>
2025-10-15 21:10:56 +0530

    Ammunition

--- Assets/Scripts/Pickups/AmmoPickup.cs
+++ Assets/Scripts/Pickups/AmmoPickup.cs
@@ -1,16 +1,10 @@
 using UnityEngine;
 
-public class AmmoPickup : MonoBehaviour
+public class AmmoPickup : Pickup
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] int ammoAmount = 100;
+    protected override void OnPickup(ActiveWeapon activeWeapon)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        activeWeapon.AdjustAmmo(ammoAmount);
     }
 }
