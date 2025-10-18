8a81eb329262750435507710f4f8dd5cbfdda663
RIZWAN <morizwan1410@gmail.com>
2025-10-18 18:00:39 +0530

    player health

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,9 +1,14 @@
+using Cinemachine;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] int startingHealth = 5;
+    [SerializeField] CinemachineVirtualCamera deathVirtualCamera;
+    [SerializeField] Transform weaponCamera;
+
     int currentHealth;
+    int gameOverVirtualCameraPriority = 20;
 
     private void Awake()
     {
@@ -17,6 +22,8 @@ public class PlayerHealth : MonoBehaviour
 
         if (currentHealth <= 0)
         {
+            weaponCamera.parent = null;
+            deathVirtualCamera.Priority = gameOverVirtualCameraPriority;
             Destroy(this.gameObject);
         }
     }
