86aa67b88fe97a985cd6ee685da075a2e47448ac
RIZWAN <morizwan1410@gmail.com>
2025-10-22 20:24:59 +0530

    turret projectile update

--- Assets/Scripts/Enemies/Turret.cs
+++ Assets/Scripts/Enemies/Turret.cs
@@ -1,12 +1,30 @@
+using System.Collections;
 using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
     [SerializeField] Transform turretHead;
     [SerializeField] Transform playerTargetPoint;
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] Transform projectileSpawnPoint;    
+    [SerializeField] float fireRate = 2f;
+
+    void Start()
+    {
+        StartCoroutine(FireRoutine());
+    }
 
     void Update()
     {
         turretHead.LookAt(playerTargetPoint);
+    }
+
+    IEnumerator FireRoutine()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(fireRate);
+            Instantiate(projectilePrefab, projectileSpawnPoint.position, transform.rotation);
+        }
     }
 }
